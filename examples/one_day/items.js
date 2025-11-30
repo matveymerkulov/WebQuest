a0e4b57c0f385948ea1cd32511920b0d6a05d787
Matvey Merkulov <32887734+matveymerkulov@users.noreply.github.com>
2025-11-30 13:52:44 +1100

    0.11.1.1 винительный падеж предметов

--- examples/one_day/items.js
+++ examples/one_day/items.js
@@ -3,41 +3,34 @@ import {Container} from "../../src/container.js"
 import {Item} from "../../src/item.js"
 
 export const laptop = combine(new Item("ноутбук"), {
-
 })
 
 export const pillow = combine(new Item("подушка"), {
-
+    name: ["подушка", "подушку"]
 })
 
 export const sheet = combine(new Item("простыня"), {
-
+    name: ["простыня", "простыню"]
 })
 
 export const blanket = combine(new Item("одеяло"), {
-
 })
 
 export const flippers = combine(new Item("тапочки"), {
-
 })
 
 export const socks = combine(new Item("носки"), {
-
 })
 
 export const boots = combine(new Item("ботинки"), {
-
 })
 
 export const suit = combine(new Item("пиджак"), {
-
 })
 
 export const shirt = combine(new Item("рубашка"), {
-
+    name: ["рубашка", "рубашку"]
 })
 
 export const trousers = combine(new Item("брюки"), {
-
 })
